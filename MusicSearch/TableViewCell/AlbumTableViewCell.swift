c73d8990b639dd61a3c3b3cb0493967465f59f01
Omar Yousef <71517267+oyousef25@users.noreply.github.com>
2021-09-25 18:32:49 -0400

    Created the classes that we are going to need to populate the Table View and add details to the Table View Cell

---
+++ MusicSearch/TableViewCell/AlbumTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  AlbumTableViewCell.swift
+//  MusicSearch
+//
+//  Created by Omar Yousef on 2021-09-25.
+//
+
+import Foundation
+import UIKit
+
+class AlbumTableViewCell: UITableViewCell{
+    //MARK: Properties
+    
+    //MARK: Methods
+}
---
+++ MusicSearch/ViewControllers/AlbumsTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AlbumsTableViewController.swift
+//  MusicSearch
+//
+//  Created by Omar Yousef on 2021-09-25.
+//
+
+import Foundation
+import UIKit
+
+class AlbumsTableViewController: UITableViewController{
+    //MARK: Properties
+    
+    //MARK: Methods
+}
