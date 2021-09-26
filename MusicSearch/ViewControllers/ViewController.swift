412f9246f0e5bf25c8dbd1673ec18f9886fbd65f
Omar Yousef <71517267+oyousef25@users.noreply.github.com>
2021-09-25 23:35:56 -0400

    Added all the needed methods to fetch data from the iTunes API

--- MusicSearch/ViewControllers/ViewController.swift
+++ MusicSearch/ViewControllers/ViewController.swift
@@ -40,6 +40,10 @@ class ViewController: UIViewController {
         This button will reset all values in the search textfield and the tableView.
      */
     @IBAction func resetButton(_ sender: Any) {
+        //Reset the search bar
+        searchBar.text = ""
+        
+        
     }
     
     
@@ -68,7 +72,7 @@ extension ViewController: UITableViewDelegate{
 //MARK: TableView Datasource
 extension ViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return albumResults.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -77,8 +81,8 @@ extension ViewController: UITableViewDataSource{
 //        cell.cellBackground.backgroundColor = UIColor(named: "CellBackground")
 //        cell.cellBackground.layer.cornerRadius = 5.0
         
-//        let movie = albumResults[indexPath.row]
-//        cell.setUpCell(using: movie)
+//        let album = albumResults[indexPath.row]
+//        cell.setUpCell(using: album)
         
         return cell
     }
@@ -90,9 +94,23 @@ extension ViewController: UISearchBarDelegate{
     /*
         If the search button was clicked
      */
-//    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-//        <#code#>
-//    }
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        //Storing the value of the user input in a variable to access it later when creating the url
+        if let text = searchBar.text, !text.isEmpty {
+            print("Searching for \(text)")
+            
+            //creating a new url using the user input in the search bar
+            if let url = createAlbumUrl(for: text){
+            
+                //Fetching the albums from the url
+                fetchAlbums(from: url)
+            }
+        } else {
+            print("Nothing entered")
+        }
+        
+        searchBar.resignFirstResponder()
+    }
     
     
     
@@ -100,10 +118,56 @@ extension ViewController: UISearchBarDelegate{
     /*
         Function to create a valid URL
      */
+    func createAlbumUrl(for album: String) -> URL?{
+        //Create the url string and return it
+        guard let cleanURL = album.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { fatalError("Can't create a valid url") }
+        
+        var urlString = "https://itunes.apple.com/search?"
+        urlString = urlString.appending("term+\(cleanURL)")
+        urlString = urlString.appending("&entity=musicTrack")
+        print(urlString)
+        
+        
+        return URL(string: urlString)
+    }
     
     
     /*
         Fetch the albums from the API
      */
+    func fetchAlbums(from url: URL){
+        //Starting a new url session to fetch data from the JSON object thats returned by our URL
+        let albumTask = URLSession.shared.dataTask(with: url){
+            data, response, error in
+            
+            //Handling any errors and diplaying them
+            if let error = error {
+                print("There was an error fetching the data - \(error.localizedDescription)")
+            } else {
+                do {
+                    //Unwrapping using optional binding to make sure its not nil
+                    guard let someData = data else { return }
+                    
+                    /*
+                        Decoding the JSON data
+                     */
+                    let jsonDecoder = JSONDecoder()
+                    let downloadedResults = try jsonDecoder.decode(Albums.self, from: someData)
+                    
+                    //Adding all the results to our albums array
+                    self.albumResults = downloadedResults.results
+                } catch let error {
+                    print("Problem decoding: \(error.localizedDescription)")
+                }
+                
+                //Reloading the tableview to display the new results
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        }
+        albumTask.resume()
+    }
+    
 }
 
