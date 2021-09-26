d067328df0ff09633bdceeed76e312518870016d
Omar Yousef <71517267+oyousef25@users.noreply.github.com>
2021-09-26 00:15:06 -0400

    Finished the table view cell's class

--- MusicSearch/TableViewCell/AlbumTableViewCell.swift
+++ MusicSearch/TableViewCell/AlbumTableViewCell.swift
@@ -9,7 +9,86 @@ import Foundation
 import UIKit
 
 class AlbumTableViewCell: UITableViewCell{
-    //MARK: Properties
+    //MARK: Outlets
+    @IBOutlet weak var cellBackground: UIView!
+    
+    @IBOutlet weak var albumTitle: UILabel!
+    
+    @IBOutlet weak var trackName: UILabel!
+    
+    @IBOutlet weak var artistName: UILabel!
+    
+    @IBOutlet weak var albumImage: UIImageView!
+    
+    @IBOutlet weak var trackCost: UILabel!
+    
+    @IBOutlet weak var albumGenre: UILabel!
+    
     
     //MARK: Methods
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    
+    /*
+        This method will replace each property value in the cell with the the ones that are returned by the object
+     */
+    func setUpCell(using album: Album){
+        //Setting the value for each of the object's properities
+        albumTitle.text = album.collectionName
+        trackName.text = album.trackName
+        artistName.text = album.artistName
+        //trackCost.text = Double(album.trackPrice) ?? 0.00
+        albumGenre.text = album.wrapperType
+        
+        //set a default image in case there is no poster
+        albumImage.image = UIImage(named: "sample")
+        
+        //get the poster path string
+        guard let posterPath = album.collectionViewURL else { return }
+        
+        //TODO: build a url to fetch the post and load the image
+        if let url = buildImageUrl(for: posterPath){
+            print(url)
+            loadPoster(url: url, forCell: self)
+        }
+    }
+    
+    /*
+        This method will be building a valid image URL
+     */
+    func buildImageUrl(for path: String) -> URL? {
+        let imagePath = path
+        
+        guard let imageURL = URL(string: imagePath) else { return nil}
+
+        return imageURL
+    }
+    
+    /*
+        This method will load the image from the URL
+     */
+    func loadPoster(url: URL, forCell cell: AlbumTableViewCell){
+        /*
+         Starting a new URL session to read the image from the url
+         */
+        let session = URLSession.shared
+        
+        let task = session.downloadTask(with: url){
+            url, response, error in
+            
+            if error == nil, let url = url, let data = try? Data(contentsOf: url), let posterImage = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    cell.albumImage.image = posterImage
+                }
+            }
+        }
+        task.resume()
+    }
 }
